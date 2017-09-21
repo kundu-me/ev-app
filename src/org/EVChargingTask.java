c856ee5684989888e158ae0471d117986ce2750e
nxkundu <Pass@1234>
2017-09-21 16:35:43 -0700

    Initial Commit fot the EV Start Stop Charging from Android app

---
+++ src/org/EVChargingTask.java
@@ -0,0 +1,69 @@
+package org;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class EVChargingTask  extends TimerTask{
+
+	private Timer startTimer;
+	private Timer stopTimer;
+	private boolean isCharge;
+
+	public EVChargingTask(Timer startTimer, Timer stopTimer, boolean isCharge) {
+
+		Date dt = new Date();
+		System.out.print("Time [" + dt.toString() + "] - ");
+		System.out.println("EVCharging Initialized ... ");
+
+		this.startTimer = startTimer;
+		this.stopTimer = stopTimer;
+		this.isCharge = isCharge;
+	}
+
+	@Override
+	public void run() {
+
+		if(isCharge) {
+			
+			Date dt = new Date();
+			System.out.print("Time [" + dt.toString() + "] - ");
+			System.out.println("Calling startCharging() ... ");
+			this.startCharging();
+		}
+		else {
+			
+			Date dt = new Date();
+			System.out.print("Time [" + dt.toString() + "] - ");
+			System.out.println("Calling stopCharging() ... ");
+			this.stopCharging();
+		}
+
+	}
+
+	private void startCharging() {
+
+		Date dt = new Date();
+		System.out.print("Time [" + dt.toString() + "] - ");
+		System.out.println("Charging Started ... ");
+		
+		// Call the function for start charging here
+	}
+
+	private void stopCharging() {
+
+		Date dt = new Date();
+		System.out.print("Time [" + dt.toString() + "] - ");
+		System.out.println("Charging Stopped ... ");
+		
+		startTimer.cancel();
+		stopTimer.cancel();
+		
+		Date dt2 = new Date();
+		System.out.print("Time [" + dt2.toString() + "] - ");
+		System.out.println("Everything Stopped ... ");
+		
+		// Call the function for stop charging here
+	}
+
+}
